9950fb34d0a70c5441b3b933de0797adf2dd10bb
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2023-08-15 21:42:19 -0400

    Todo on all lines

--- group_anagrams/main.py
+++ group_anagrams/main.py
@@ -1,11 +1,10 @@
 from typing import List
 from collections import defaultdict
 
-'''
-TODO For loop hashmap then save chars dict with
-ints indicating the amount needed. Inside with a list of words then
-extract the lists of words connected to the chars dict return.
-'''
+
+#TODO For loop hashmap then save chars dict with
+#TODO ints indicating the amount needed. Inside with a list of words then
+#TODO extract the lists of words connected to the chars dict return.
 
 
 def group_anagrams(strs: List[str]) -> List[List[str]]:
